b48d14265f23dd4ef13c05ed0c061aee6be1f0ec
the-Shallow <theshallow27@gmail.com>
2021-07-20 22:06:00 +0530

    updated code

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('uncaughtException');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -23,4 +29,13 @@ mongoose
 const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log('Listening to port:' + port);
+});
+
+
+process.on('unhandledRejection', (err) => {
+  console.log('unhandledRejection');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
