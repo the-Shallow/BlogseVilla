0aba06155bedaa1eea68d83cad80161187d85c63
the-Shallow <theshallow27@gmail.com>
2021-07-22 15:32:33 +0530

    Updated login.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8608,7 +8608,7 @@ var logout = /*#__PURE__*/function () {
 
           case 3:
             res = _context2.sent;
-            if (res.data.status = 'success') location.reload(true);
+            if (res.data.status = 'success') location.assign('/');
             _context2.next = 10;
             break;
 
--- public/js/login.js
+++ public/js/login.js
@@ -29,7 +29,7 @@ export const logout = async () => {
       method: 'GET',
       url: '/api/v1/users/logout',
     });
-    if ((res.data.status = 'success')) location.reload(true);
+    if ((res.data.status = 'success')) location.assign('/');
   } catch (err) {
     console.log(err);
   }
